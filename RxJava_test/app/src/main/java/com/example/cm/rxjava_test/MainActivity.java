c8ff34258d622ab43690aa7bfeb08f47138b4cc1
buder <chenpeng3@cmcm.com>
2018-09-26 22:45:50 +0800

    RxJava学习项目

---
+++ RxJava_test/app/src/main/java/com/example/cm/rxjava_test/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.cm.rxjava_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        syncTest();
+
+    }
+
+    private void syncTest() {
+        Observable observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onComplete();
+            }
+        });
+
+        Observer observer = new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Object o) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        observable.subscribe(observer);
+
+    }
+}
