1c88fe854f4b005d91607adf47cbb11255cc8d0e
chenpeng02 <chenpeng02@qiyi.com>
2020-03-31 20:41:10 +0800

    activity uri跳转

---
+++ URIJump/app/src/main/java/com/test/urijump/Main2Activity.java
@@ -0,0 +1,32 @@
+package com.test.urijump;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        if (null != intent) {
+            Uri uri = intent.getData();
+            if (uri == null) {
+                return;
+            }
+            String actionData = uri.getQueryParameter("action");
+            String secretData = uri.getQueryParameter("secret");
+
+            TextView tv = findViewById(R.id.txt);
+            tv.append("\n传过来的action值为:" + actionData);
+            tv.append("\n传过来的secret值为:" + secretData);
+        }
+
+    }
+}
---
+++ URIJump/app/src/main/java/com/test/urijump/MainActivity.java
@@ -0,0 +1,54 @@
+package com.test.urijump;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**
+                 * 方式一：类名跳转
+                 */
+//                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+//                startActivity(intent);
+
+
+                /**
+                 * 方式二：action跳转
+                 */
+
+//                Intent intent1 = new Intent("self_define_action");
+//                startActivity(intent1);
+
+
+                /**
+                 * 方式三：URI跳转
+                 */
+                String uriString = "hello://test.uri.activity?action=123&secret=buder";
+                Uri uri = Uri.parse(uriString);
+                Intent intent = new Intent("self_define_uri");
+                //Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(uri);
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+}
