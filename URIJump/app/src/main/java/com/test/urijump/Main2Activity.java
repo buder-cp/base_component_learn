6d890a1815a0adc00edd3fff1e0c7dcc10540a7a
chenpeng02 <chenpeng02@qiyi.com>
2020-04-01 13:43:28 +0800

    intent bundle方式传参

--- URIJump/app/src/main/java/com/test/urijump/Main2Activity.java
+++ URIJump/app/src/main/java/com/test/urijump/Main2Activity.java
@@ -15,17 +15,27 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         Intent intent = getIntent();
+//        if (null != intent) {
+//            Uri uri = intent.getData();
+//            if (uri == null) {
+//                return;
+//            }
+//            String actionData = uri.getQueryParameter("action");
+//            String secretData = uri.getQueryParameter("secret");
+//
+//            TextView tv = findViewById(R.id.txt);
+//            tv.append("\n传过来的action值为:" + actionData);
+//            tv.append("\n传过来的secret值为:" + secretData);
+//        }
+
         if (null != intent) {
-            Uri uri = intent.getData();
-            if (uri == null) {
-                return;
-            }
-            String actionData = uri.getQueryParameter("action");
-            String secretData = uri.getQueryParameter("secret");
+            Bundle extras = intent.getExtras();
+            String value1 = extras.getString("key1");
+            String value2 = extras.getString("key2");
 
             TextView tv = findViewById(R.id.txt);
-            tv.append("\n传过来的action值为:" + actionData);
-            tv.append("\n传过来的secret值为:" + secretData);
+            tv.append("\n传过来的key1值为:" + value1);
+            tv.append("\n传过来的key2值为:" + value2);
         }
 
     }
--- URIJump/app/src/main/java/com/test/urijump/MainActivity.java
+++ URIJump/app/src/main/java/com/test/urijump/MainActivity.java
@@ -29,22 +29,26 @@ public class MainActivity extends AppCompatActivity {
 
 
                 /**
-                 * 方式二：action跳转
+                 * 方式二：action跳转，bundle传值
                  */
 
-//                Intent intent1 = new Intent("self_define_action");
-//                startActivity(intent1);
+                Intent intent1 = new Intent("self_define_action");
+                Bundle bundle = new Bundle();
+                bundle.putString("key1", "value111");
+                bundle.putString("key2", "value222");
+                intent1.putExtras(bundle);
+                startActivity(intent1);
 
 
                 /**
-                 * 方式三：URI跳转
+                 * 方式三：URI跳转，query传值
                  */
-                String uriString = "hello://test.uri.activity?action=123&secret=buder";
-                Uri uri = Uri.parse(uriString);
-                Intent intent = new Intent("self_define_uri");
-                //Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(uri);
-                startActivity(intent);
+//                String uriString = "hello://test.uri.activity?action=123&secret=buder";
+//                Uri uri = Uri.parse(uriString);
+//                Intent intent = new Intent("self_define_uri");
+//                //Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(uri);
+//                startActivity(intent);
 
 
             }
