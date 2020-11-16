8e111427347db1d34ef9f5a73f2350f1f1f7bc84
chenpeng02 <chenpeng02@qiyi.com>
2020-11-16 16:14:22 +0800

    working：监听应用的卸载安装

--- IQWork/app/src/main/java/com/example/iqtest/MainActivity.java
+++ IQWork/app/src/main/java/com/example/iqtest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.iqtest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button heapTestBtn;
     Button signalBtn;
 
+    private MyInstalledReceiver installedReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.e("MainActivity", "onCreate");
@@ -29,6 +34,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         signalBtn = findViewById(R.id.signal_info);
         heapTestBtn.setOnClickListener(this);
         signalBtn.setOnClickListener(this);
+
+        installedReceiver = new MyInstalledReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.intent.action.PACKAGE_ADDED");
+        filter.addAction("android.intent.action.PACKAGE_REMOVED");
+        filter.addAction("android.intent.action.PACKAGE_REPLACED");
+        filter.addDataScheme("package");
+        registerReceiver(installedReceiver, filter);
 
         /**
          * 长虹二期，过滤获取能通过图标启动的 系统应用、用户安装的三方应用
@@ -60,6 +73,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(installedReceiver != null) {
+            this.unregisterReceiver(installedReceiver);
+        }
+    }
+
+    @Override
     public void onClick(View view) {
         if (view.getId() == R.id.native_heap_test) {
 //            Intent intent = new Intent(MainActivity.this, NativeHeapActivity.class);
@@ -71,4 +92,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             SignalTest.getSignalNew(this);
         }
     }
+
+    class MyInstalledReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals("android.intent.action.PACKAGE_ADDED")) {		// install
+                String packageName = intent.getDataString();
+                Log.i("homer", "安装了 :" + packageName);
+            }
+
+            if (intent.getAction().equals("android.intent.action.PACKAGE_REMOVED")) {	// uninstall
+                String packageName = intent.getDataString();
+                Log.i("homer", "卸载了 :" + packageName);
+            }
+        }
+    }
+
 }
