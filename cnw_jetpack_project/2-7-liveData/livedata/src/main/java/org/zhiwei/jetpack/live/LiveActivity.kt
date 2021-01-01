e90e0970ae42f0209484779becb97bfccd6f4416
chenpeng02 <chenpeng02@qiyi.com>
2021-01-01 18:59:33 +0800

    cnw_2-7-liveData简单示例（二）

--- cnw_jetpack_project/2-7-liveData/livedata/src/main/java/org/zhiwei/jetpack/live/LiveActivity.kt
+++ cnw_jetpack_project/2-7-liveData/livedata/src/main/java/org/zhiwei/jetpack/live/LiveActivity.kt
@@ -14,6 +14,11 @@ class LiveActivity : AppCompatActivity() {
     //声明变量live data
     val liveAppleData = MutableLiveData<String>()
 
+    //中介者MediatorLiveData使用演示：
+    val liveOne = MutableLiveData<String>()
+    val liveTwo = MutableLiveData<String>()
+    val mediatorLive = MediatorLiveData<Pair<String, String>>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_livedata)
@@ -47,6 +52,37 @@ class LiveActivity : AppCompatActivity() {
         liveAppleData.observe(this, Observer {
             tv_live_data_activity.text = it
             Log.d("LiveActivity", "LiveData在LiveActivity中 $it")
+        })
+
+        //map转换后的数值：
+        val liveMappedData = liveAppleData.map {
+            Pair<Int, String>(it.hashCode(), it)
+        }
+
+        liveMappedData.observe(this, Observer {
+            tv_mapped_data_activity.text = "map 后的值：${it}"
+            Log.i("LiveActivity", "LiveData在LiveActivity中 map 后 $it")
+        })
+
+        //中介者MediatorLiveData使用演示：
+        btn_change1_live.setOnClickListener {
+            liveOne.value = "one:${System.currentTimeMillis().toString().takeLast(6)}"
+        }
+        btn_change2_live.setOnClickListener {
+            liveTwo.value = "two:${System.currentTimeMillis().toString().takeLast(6)}"
+        }
+
+        mediatorLive.addSource(liveTwo) {
+            Log.d("LiveActivity", "LiveActivity中 LiveTwo ---> $it")
+            mediatorLive.value = "two >>>>>" to it
+        }
+        mediatorLive.addSource(liveOne) {
+            Log.d("LiveActivity", "LiveActivity中 LiveOne ---> $it")
+            mediatorLive.value = "one >>" to it
+        }
+
+        mediatorLive.observe(this, Observer {
+            Log.w("LiveActivity", "LiveActivity中 mediatorLive ---> $it")
         })
     }
 
