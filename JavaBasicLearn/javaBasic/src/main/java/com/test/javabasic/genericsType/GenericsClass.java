a6c21cda14301e26f09705ef8f19a37b5947629e
chenpeng02 <chenpeng02@qiyi.com>
2021-02-07 14:59:23 +0800

    java泛型

--- JavaBasicLearn/javaBasic/src/main/java/com/test/javabasic/genericsType/GenericsClass.java
+++ JavaBasicLearn/javaBasic/src/main/java/com/test/javabasic/genericsType/GenericsClass.java
@@ -1,5 +1,12 @@
 package com.test.javabasic.genericsType;
 
+import com.sun.org.apache.xerces.internal.xs.StringList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sun.net.www.content.text.Generic;
+
 //泛型类定义
 //在泛型类、泛型接口的方法中，把泛型中声明的类型形参当成普通类型使用
 public class GenericsClass<K, V> {
@@ -48,11 +55,25 @@ class UseTest {
 //当创建了带泛型声明的接口、父类之后，可以为该接口创建实现类，
 // 或者从该父类派生子类，需要注意：使用这些接口、父类派生子类时不能再包含类型形参，
 // 需要传入具体的类型。
-class A extends GenericsClass<String, String> {
+// 保留父类泛型 ----》泛型子类
+// 1）全部保留
+class A<K, V> extends GenericsClass<K, V> {
 
 }
 
-//也可以不指定具体的类型，如下：
-class B extends GenericsClass {
+// 2) 部分保留
+class B<K> extends GenericsClass<K, String> {
+
+}
+
+// 不保留父类泛型 -----》子类按需实现
+// 1)具体类型
+class C extends GenericsClass<String, Integer> {
+
+}
+
+// 2)没有具体类型
+// 泛型擦除：实现或继承父类的子类，没有指定类型，类似于Object
+class D extends GenericsClass {
 
 }
