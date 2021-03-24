21153223c4af2567baa51400b8378a4bf80e1e20
chenpeng02 <chenpeng02@qiyi.com>
2021-03-24 11:11:55 +0800

    service 与activity通信

--- android_ipc/service/src/main/java/com/example/service/MainActivity.java
+++ android_ipc/service/src/main/java/com/example/service/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.service;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +14,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button startService;
     private Button stopService;
+    private Button bindService;
+    private Button unbindService;
+
+    private MyService.MyBinder myBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        //重写onServiceConnected()方法和onServiceDisconnected()方法
+        //在Activity与Service建立关联和解除关联的时候调用
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            //实例化Service的内部类myBinder
+            //通过向下转型得到了MyBinder的实例
+            myBinder = (MyService.MyBinder) service;
+            //在Activity调用Service类的方法
+            myBinder.service_connect_Activity();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +44,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         startService = findViewById(R.id.startService);
         stopService = findViewById(R.id.stopService);
+        bindService = findViewById(R.id.bindService);
+        unbindService = findViewById(R.id.unbindService);
 
         startService.setOnClickListener(this);
         stopService.setOnClickListener(this);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
     }
 
     @Override
@@ -42,6 +71,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent stopIntent = new Intent(this, MyService.class);
                 //调用stopService()方法-传入Intent对象,以此停止服务
                 stopService(stopIntent);
+                break;
+
+            //点击绑定Service
+            case R.id.bindService:
+                //构建绑定服务的Intent对象
+                Intent bindIntent = new Intent(this, MyService.class);
+                //调用bindService()方法,以此停止服务
+                boolean isBindServiceSuccess = bindService(bindIntent, connection, BIND_AUTO_CREATE);
+                System.out.println("isBindServiceSuccess:" + isBindServiceSuccess);
+                //参数说明
+                //第一个参数:Intent对象
+                //第二个参数:上面创建的Serviceconnection实例
+                //第三个参数:标志位
+                //这里传入BIND_AUTO_CREATE表示在Activity和Service建立关联后自动创建Service
+                //这会使得MyService中的onCreate()方法得到执行，但onStartCommand()方法不会执行
+                break;
+
+            //点击解绑Service
+            case R.id.unbindService:
+                //调用unbindService()解绑服务
+                //参数是上面创建的Serviceconnection实例
+                unbindService(connection);
                 break;
 
             default:
--- android_ipc/service/src/main/java/com/example/service/MyService.java
+++ android_ipc/service/src/main/java/com/example/service/MyService.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
@@ -15,7 +16,32 @@ import androidx.annotation.Nullable;
  * 2021-03-24 10:48:59.881 12458-12458/com.example.service I/System.out: 执行了onStartCommand()
  * 2021-03-24 10:49:01.390 12458-12458/com.example.service I/System.out: 执行了onDestory()
  */
+
+
+/**
+ * 启动服务多次点击：多次执行onStartCommand()
+ * 绑定服务多次点击：只执行一次onBind()，onServiceConnected()也只会执行一次
+ * 停止服务点击：如果没有点击解绑，则不会执行onDestory()
+ * 解绑服务点击：执行onUnbind() -> onDestory()
+ *
+ * 2021-03-24 11:01:33.037 16381-16381/com.example.service I/System.out: 执行了onCreat()
+ * 2021-03-24 11:01:33.038 16381-16381/com.example.service I/System.out: 执行了onStartCommand()
+ * 2021-03-24 11:01:34.600 16381-16381/com.example.service I/System.out: 执行了onStartCommand()
+ * 2021-03-24 11:01:35.201 16381-16381/com.example.service I/System.out: 执行了onStartCommand()
+ *
+ * 2021-03-24 11:01:37.064 16381-16381/com.example.service I/System.out: isBindServiceSuccess:true
+ * 2021-03-24 11:01:37.073 16381-16381/com.example.service I/System.out: 执行了onBind()
+ * 2021-03-24 11:01:37.077 16381-16381/com.example.service I/System.out: Service关联了Activity,并在Activity执行了Service的方法
+ * 2021-03-24 11:01:51.216 16381-16381/com.example.service I/System.out: isBindServiceSuccess:true
+ * 2021-03-24 11:01:53.801 16381-16381/com.example.service I/System.out: isBindServiceSuccess:true
+ *
+ * 2021-03-24 11:02:00.925 16381-16381/com.example.service I/System.out: 执行了onUnbind()
+ * 2021-03-24 11:02:00.926 16381-16381/com.example.service I/System.out: 执行了onDestory()
+ */
 public class MyService extends Service {
+
+    private MyBinder mBinder = new MyBinder();
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -26,8 +52,6 @@ public class MyService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         System.out.println("执行了onStartCommand()");
         return super.onStartCommand(intent, flags, startId);
-
-
     }
 
     @Override
@@ -39,6 +63,21 @@ public class MyService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        System.out.println("执行了onBind()");
+        //返回实例
+        return mBinder;
     }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        System.out.println("执行了onUnbind()");
+        return super.onUnbind(intent);
+    }
+
+    class MyBinder extends Binder {
+        public void service_connect_Activity() {
+            System.out.println("Service关联了Activity,并在Activity执行了Service的方法");
+        }
+    }
+
 }
