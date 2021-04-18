2e9467fe162f22ba87c14df3827d4510e7e52779
chenpeng02 <chenpeng02@qiyi.com>
2021-04-18 13:48:29 +0800

    【ppjoke】相关编译报错解决

--- ppjoke/app/src/main/java/com/test/ppjoke/ui/notifications/NotificationsFragment.java
+++ ppjoke/app/src/main/java/com/test/ppjoke/ui/notifications/NotificationsFragment.java
@@ -23,7 +23,7 @@ public class NotificationsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+                new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
