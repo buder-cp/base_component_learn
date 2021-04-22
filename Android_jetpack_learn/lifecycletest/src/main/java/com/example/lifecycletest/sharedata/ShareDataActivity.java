9cd4cacd78abfaf9afc9f3f7369ef8ff1cf8bb32
chenpeng02 <chenpeng02@qiyi.com>
2021-04-22 11:14:48 +0800

    【jetpack】viewModel livedata 学习示例*2

---
+++ Android_jetpack_learn/lifecycletest/src/main/java/com/example/lifecycletest/sharedata/ShareDataActivity.java
@@ -0,0 +1,17 @@
+package com.example.lifecycletest.sharedata;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lifecycletest.R;
+
+
+public class ShareDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_data);
+    }
+}
